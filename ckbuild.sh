161beb9ceb1070601df3f1208c280082544b2fda
Flopster101 <nahuelgomez329@gmail.com>
2026-01-16 12:06:13 -0300

    FLOPPY: Add "NOSUS" label to SukiSU builds
    Currently without susfs.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- ckbuild.sh
+++ ckbuild.sh
@@ -252,7 +252,7 @@ if [[ "$DO_KSU" == "1" ]]; then
     CK_TYPE="KSUNext-NOSUS"
     CK_TYPE_SHORT="KN"
 elif [ "$DO_SUKI" == "1" ]; then
-    CK_TYPE="SukiSU-Ultra"
+    CK_TYPE="SukiSU-Ultra-NOSUS"
     CK_TYPE_SHORT="SKS"
 elif [ "$DO_RKSU" == "1" ]; then
     CK_TYPE="RKSU"
