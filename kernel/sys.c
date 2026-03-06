7a98e84a168fbe53a2c931dd6b873308cc1c4904
Flopster101 <nahuelgomez329@gmail.com>
2026-03-06 12:22:16 -0300

    Revert "[PATCH] treewide: Implement susfs v2.0.0"
    Old.
    
    This reverts commit 34c651104fc3e458a21f3430da2a4377a5a7780d.
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -596,10 +596,6 @@ error:
  * This function implements a generic ability to update ruid, euid,
  * and suid.  This allows you to implement the 4.4 compatible seteuid().
  */
-#ifdef CONFIG_KSU_SUSFS
-extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
-#endif
-
 SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 {
 	struct user_namespace *ns = current_user_ns();
@@ -612,11 +608,6 @@ SYSCALL_DEFINE3(setresuid, uid_t, ruid, uid_t, euid, uid_t, suid)
 	keuid = make_kuid(ns, euid);
 	ksuid = make_kuid(ns, suid);
 
-#ifdef CONFIG_KSU_SUSFS
-	if (ksu_handle_setresuid(ruid, euid, suid)) {
-		pr_info("Something wrong with ksu_handle_setresuid()/n");
-	}
-#endif
 	if ((ruid != (uid_t) -1) && !uid_valid(kruid))
 		return -EINVAL;
 
@@ -1214,9 +1205,6 @@ static int override_version(struct new_utsname __user *name)
 #endif
 }
 
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1225,10 +1213,6 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-	susfs_spoof_uname(&tmp);
-#endif
-
 #ifndef CONFIG_FAKE_UNAME_NONE
 		int bpf_spoof = is_bpf_spoof_enabled();
 		if (bpf_spoof) {
